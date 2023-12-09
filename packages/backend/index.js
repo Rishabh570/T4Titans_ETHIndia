607a6ff7df7b23db5dd142b9b5c833cff44e8225
Rishabh Rawat <rishabh.rawat@smallcase.com>
2023-12-09 06:49:37 +0530

    fix: listen to 0.0.0.0

--- packages/backend/index.js
+++ packages/backend/index.js
@@ -1,7 +1,7 @@
 const app = require('./src/app');
 const { PORT } = require('./config');
 
-const server = app.listen(PORT, () => {
+const server = app.listen(PORT, '0.0.0.0', () => {
   console.log(`🎉🎉🎉 Application running on port: ${PORT} 🎉🎉🎉`);
 });
 
